c85ef68d6528208fc6f1312fc26c00907bdd64c0
Levy <levys@wlu.edu>
2019-10-31 19:11:22 -0400

    Added headers with license info

--- a3c.py
+++ a3c.py
@@ -1,3 +1,24 @@
+'''
+A3C module
+
+Adapted from https://github.com/tensorflow/models/blob/master/research/a3c_blogpost/a3c_cartpole.py
+
+Copyright (C) Simon D. Levy 2019
+
+This file is part of A3C.
+
+A3C is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as 
+published by the Free Software Foundation, either version 3 of the 
+License, or (at your option) any later version.
+This code is distributed in the hope that it will be useful,     
+but WITHOUT ANY WARRANTY without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU Lesser General Public License 
+along with this code.  If not, see <http:#www.gnu.org/licenses/>.
+'''
+
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = ''
 
--- gym_ac3.py
+++ gym_ac3.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
+'''
+Test script for A3C module.
+
+Adapted from https://github.com/tensorflow/models/blob/master/research/a3c_blogpost/a3c_cartpole.py
+
+Copyright (C) Simon D. Levy 2019
+
+This file is part of A3C.
+
+A3C is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as 
+published by the Free Software Foundation, either version 3 of the 
+License, or (at your option) any later version.
+This code is distributed in the hope that it will be useful,     
+but WITHOUT ANY WARRANTY without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU Lesser General Public License 
+along with this code.  If not, see <http:#www.gnu.org/licenses/>.
+'''
+
 import os
 import argparse
 import gym
